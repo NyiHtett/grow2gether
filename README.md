85e22f63ee91b221462bbecf36ffd32fd3a64661
Nyi <nyihtet4dev@icloud.com>
2026-06-24 13:25:54 -0700

    June 24

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # grow2gether
+
+initial structure
+
+grow2gether/
+├── frontend/
+│   ├── src/
+│   └── package.json
+│
+├── backend/
+│   ├── app.py
+│   ├── requirements.txt
+│   └── venv/
+│
+└── README.md
+
---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+@app.route('/')
+def imageSave():
+    return "Our Memories together through the calender"
+
+@app.route('/diet')
+def diet():
+    return "Track your calories"
+
+@app.route('/appreciate')
+def appreciate():
+    return "One thing you appreciate for today"
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
