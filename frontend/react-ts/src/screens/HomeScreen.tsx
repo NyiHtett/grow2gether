a3f5b0e64b2c5cb5fdeca5eecfdbf4edd7a3cb50
Nyi <nyihtet4dev@icloud.com>
2026-07-05 12:20:57 -0700

    test

--- frontend/react-ts/src/screens/HomeScreen.tsx
+++ frontend/react-ts/src/screens/HomeScreen.tsx
@@ -36,6 +36,7 @@ export function HomeScreen({ go }: { go: (r: Route) => void }) {
       }) 
       
       const data = await res.json()
+      console.log(data)
       setLink(`${import.meta.env.VITE_API_URL}/invite/${data.inviteCode}`) // set the link state to the invite link
       // construct sign up link for the other user
     }
