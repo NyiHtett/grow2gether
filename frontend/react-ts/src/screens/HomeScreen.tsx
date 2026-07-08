c167e5b2ab1150a2e4af070a91add656077d95ad
Nyi <nyihtet4dev@icloud.com>
2026-07-08 10:00:36 -0700

    messages fixed

--- frontend/react-ts/src/screens/HomeScreen.tsx
+++ frontend/react-ts/src/screens/HomeScreen.tsx
@@ -11,7 +11,7 @@ const FEATURES: {
   Icon: ComponentType<SVGProps<SVGSVGElement>>;
 }[] = [
   { route: "photos", label: "Photo Dump", Icon: CameraIcon },
-  // { route: "study", label: "Study", Icon: BookIcon },
+  { route: "study", label: "Study", Icon: BookIcon },
   { route: "thoughts", label: "Something I didn't say", Icon: BrainIcon },
 ];
 
